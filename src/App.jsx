f086231caa7bc1064c0d6ced5a8f4a57869e93e4
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-03-06 09:07:14 +0000

    fixed route in App.js, fixed line base in vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>        
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />      
       </Routes>
