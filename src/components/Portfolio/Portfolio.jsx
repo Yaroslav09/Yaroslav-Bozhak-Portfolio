4c85625f3886e31c8f66f01f83aad7194f4f08c1
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-03-07 08:46:27 +0000

    added one card project to portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import dimifua from '../.././assets/item-1.png';
 import logo from '../.././assets/logo.png';
 import myLogo from '../.././assets/myLogo.svg';
 
@@ -19,14 +20,14 @@ function Portfolio() {
       </Card>
 
       <Card style={{ width: '18rem', marginBottom: '1rem' }}>
-        <Card.Img variant="top" src={myLogo} />
+        <Card.Img variant="top" src={dimifua} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Comfort House</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            The site of the utility company
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" href='https://dimif.if.ua/' target='blank'>Wisit site</Button>
+          {/* <Button variant="primary" href='https://dimif.if.ua/' target='blank'>Source</Button> */}
         </Card.Body>
       </Card>
 
