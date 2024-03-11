1a19a8a284fd21d5fccfb726a8a581032a175d91
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-03-11 22:02:17 +0000

    fixed some issue with rotes, chemged routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
@@ -7,17 +7,35 @@ import ContactUs from './components/ContactUs/ContactUs';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
-function App() {
-  return( 
-    <Router>
-      <Header />
-      <Routes>                
-        <Route exact path="/Yaroslav-Bozhak-Portfolio/" element={<Home />} />  
-        <Route path="/Yaroslav-Bozhak-Portfolio/about" element={<About />} />
-        <Route path="/Yaroslav-Bozhak-Portfolio/portfolio" element={<Portfolio />} />     
-        <Route path="/Yaroslav-Bozhak-Portfolio/contact" element={<ContactUs />} />  
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<ContactUs />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
       </Routes>
-    </Router>  
+    </BrowserRouter>
   );
 }
-export default App
+
+
+// function App() {
+//   return( 
+//     <Router>
+//       <Header />
+//       <Routes>                
+//         <Route index element={<Home />} />  
+//         <Route path="Yaroslav-Bozhak-Portfolio/about" element={<About />} />
+//         <Route path="Yaroslav-Bozhak-Portfolio/portfolio" element={<Portfolio />} />     
+//         <Route path="Yaroslav-Bozhak-Portfolio/contact" element={<ContactUs />} />  
+//       </Routes>
+//     </Router>  
+//   );
+// }
+// export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,52 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import {Outlet, Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to="/Yaroslav-Bozhak-Portfolio/">YB Yaroslav Bozhak</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" >
-            <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/about">About</Nav.Link>
-            <Nav.Link href="#link">Skills</Nav.Link>
-            <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/portfolio">Portfolio</Nav.Link>            
-            <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/contact">Contact</Nav.Link>    
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">YB Yaroslav Bozhak</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" >
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link href="#link">Skills</Nav.Link>
+              <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>            
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>    
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
   );
 }
+
+// function Header() {
+//   return (
+//     <>
+//       <Navbar expand="lg" className="bg-body-tertiary">
+//         <Container>
+//           <Navbar.Brand as={Link} to="/Yaroslav-Bozhak-Portfolio/">YB Yaroslav Bozhak</Navbar.Brand>
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="me-auto" >
+//               <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/">Home</Nav.Link>
+//               <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/about">About</Nav.Link>
+//               <Nav.Link href="#link">Skills</Nav.Link>
+//               <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/portfolio">Portfolio</Nav.Link>            
+//               <Nav.Link as={Link} to="/Yaroslav-Bozhak-Portfolio/contact">Contact</Nav.Link>    
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//       <Outlet />
+//     </>
+//   );
+// }
 
 export default Header;
